e58ab7d8602ccb166b9c26d90ff889fb12b53077
Nicholas Butts <nlbutts@ieee.org>
2025-01-11 17:05:37 -0600

    Feature/mechanical advantage2 (#2)
    * Resynced with a different code base.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+// Copyright (c) 2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * (log replay from a file).
+ */
+public final class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
+}
--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RedRiverRage2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "1fa4c4abaa3e0b44185837ed4651aa40939937a5";
-  public static final String GIT_DATE = "2025-01-05 19:39:28 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-07 13:42:23 EST";
-  public static final long BUILD_UNIX_TIME = 1736275343643L;
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "4f6f6f17aa14ebf44c570b30618d344232993fb9";
+  public static final String GIT_DATE = "2025-01-11 17:53:05 EST";
+  public static final String GIT_BRANCH = "feature/mechanical_advantage2";
+  public static final String BUILD_DATE = "2025-01-11 18:05:07 EST";
+  public static final long BUILD_UNIX_TIME = 1736636707506L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
