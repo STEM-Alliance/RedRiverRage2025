4389ff44f329a3ca2e08cfffc5a29ea13d04f3ab
Nick Butts <nlbutts@ieee.org>
2025-01-14 17:03:20 -0600

    Updated code to fix build issues.

--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -1,19 +1,24 @@
+// Copyright (c) 2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package org.littletonrobotics.frc2025;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RedRiverRage2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "4f6f6f17aa14ebf44c570b30618d344232993fb9";
-  public static final String GIT_DATE = "2025-01-11 17:53:05 EST";
-  public static final String GIT_BRANCH = "feature/mechanical_advantage2";
-  public static final String BUILD_DATE = "2025-01-11 18:05:07 EST";
-  public static final long BUILD_UNIX_TIME = 1736636707506L;
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "e58ab7d8602ccb166b9c26d90ff889fb12b53077";
+  public static final String GIT_DATE = "2025-01-11 18:05:37 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-14 18:02:58 EST";
+  public static final long BUILD_UNIX_TIME = 1736895778110L;
   public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
